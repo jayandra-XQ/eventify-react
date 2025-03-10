7fb90086601768c9a45cd71e9684898dc3c7c6c1
Jayandra Hamal <salonhamal278@gmail.com>
2025-03-10 11:08:59 +0545

    added .env file

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import express from 'express';
 
 const app = express();
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
