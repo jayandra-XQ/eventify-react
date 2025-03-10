ce2b2c98d3679426ce5e4e3dc5d75d837d3f4928
Jayandra Hamal <salonhamal278@gmail.com>
2025-03-10 22:09:31 +0545

    create model for user

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-})
-
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
   connectDB()
