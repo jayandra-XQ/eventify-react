f2dd8fe0417dd2f11999ba524f60555841961308
Jayandra Hamal <salonhamal278@gmail.com>
2025-03-11 15:02:38 +0545

    create signup and login page UI

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const login = () => {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('form data submitted', formData)
+
+    navigate('/')
+  }
+
   return (
     <div className="bg-gradient-to-br from-blue-100 to-blue-200 min-h-screen flex flex-col">
       <div className="container mx-auto px-4 py-8 flex-grow flex items-center justify-center">
@@ -10,21 +27,32 @@ const login = () => {
           <h2 className="text-4xl font-semibold text-center mb-8">Event Login</h2>
           <hr />
 
-          <form className="space-y-8 mt-10">
+          <form className="space-y-8 mt-10" onSubmit={handleSubmit}>
             <div>
               <label className="block text-sm font-medium text-gray-700">Email</label>
-              <input type="email" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg" placeholder="abcdef1234@gmail.com" />
+              <input
+                type="email"
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg" placeholder="abcdef1234@gmail.com"
+                id='email'
+                value={formData.email}
+                onChange={handleChange}
+              />
             </div>
 
             <div>
               <label className="block text-sm font-medium text-gray-700">Password</label>
-              <input type="password" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
+              <input
+                type="password"
+                className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
                 placeholder='************'
+                id='password'
+                value={formData.password}
+                onChange={handleChange}
               />
             </div>
             <div>
               <button className="w-30% bg-indigo-600 text-white py-3 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-lg"
-                onClick={() => navigate('/')}
+                type='submit'
               >
                 Login
               </button>
--- frontend/src/pages/signupPage.jsx
+++ frontend/src/pages/signupPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const signupPage = () => {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    gender: ''
+  })
+
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("form data submitted", formData)
+
+    navigate('/login')
+  }
 
   return (
     <div className="bg-gradient-to-br from-blue-100 to-blue-200 min-h-screen flex flex-col">
@@ -12,36 +32,62 @@ const signupPage = () => {
             <h2 className="text-4xl font-semibold text-center mb-8">Event Registration</h2>
             <hr />
 
-            <form className="space-y-8 mt-10">
+            <form className="space-y-8 mt-10" onSubmit={handleSubmit}>
               <div>
                 <label className="block text-sm font-medium text-gray-700">First Name</label>
-                <input type="text" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg "
+                <input
+                  type="text"
+                  className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg "
                   placeholder='First Name'
+                  id='firstName'
+                  value={formData.firstName}
+                  onChange={handleChange}
                 />
               </div>
 
               <div>
                 <label className="block text-sm font-medium text-gray-700">Last Name</label>
-                <input type="text" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
+                <input
+                  type="text"
+                  className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
                   placeholder='Last Name'
+                  id='lastName'
+                  value={formData.lastName}
+                  onChange={handleChange}
                 />
               </div>
 
               <div>
                 <label className="block text-sm font-medium text-gray-700">Email</label>
-                <input type="email" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg" placeholder="abcdef1234@gmail.com" />
+                <input
+                  type="email"
+                  className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg" placeholder="abcdef1234@gmail.com"
+                  id='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                />
               </div>
 
               <div>
                 <label className="block text-sm font-medium text-gray-700">Password</label>
-                <input type="password" className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
+                <input
+                  type="password"
+                  className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
                   placeholder='************'
+                  id='password'
+                  value={formData.password}
+                  onChange={handleChange}
                 />
               </div>
 
               <div>
                 <label className="block text-sm font-medium text-gray-700">Gender</label>
-                <select className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg">
+                <select
+                  className="mt-1 block w-full border-gray-300 rounded-md shadow-md focus:ring-indigo-500 focus:border-indigo-500 p-3 text-lg"
+                  id='gender'
+                  value={formData.gender}
+                  onChange={handleChange}
+                >
                   <option>----Select----</option>
                   <option>Male</option>
                   <option>Female</option>
@@ -51,7 +97,7 @@ const signupPage = () => {
 
               <div>
                 <button className="w-30% bg-indigo-600 text-white py-3 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 text-lg"
-                  onClick={() => navigate('/login')}
+                  type='submit'
                 >
                   Register
                 </button>
