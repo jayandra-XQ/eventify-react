a8a8d7f5565e9c441351b1591b21c3b8a9ed1b40
Jayandra Hamal <salonhamal278@gmail.com>
2025-03-14 21:27:23 +0545

    add persist gate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
