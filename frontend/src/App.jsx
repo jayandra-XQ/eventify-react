aa22f6506c8d627ab7291ba1978daadd9fcf5034
Jayandra Hamal <salonhamal278@gmail.com>
2025-03-20 23:04:04 +0545

    fix: App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import UserLayout from "./layouts/UserLayout";
+import AdminLayout from "./layouts/AdminLayout";
+
+// Public Pages
 import Home from "./pages/homePage";
 import Signup from "./pages/signupPage";
 import Login from "./pages/loginPage";
 import About from "./pages/aboutPage";
 import Category from "./pages/categoryPage";
+import UserBook from "./pages/Users/UserBook";
+import UserBookinghistory from "./pages/Users/UserBookinghistory";
+import Contact from "./pages/Users/contact";
+import Userprofile from "./pages/Users/Userprofile";
+import Payment from "./pages/Users/Payment";
 
-// Admin Components
+// Admin Pages
 import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminNavbar from "./components/AdminNavbar";
 import UserList from "./pages/admin/UserList";
 import Adminabout from "./pages/admin/Adminabout";
 import Bookinghistory from "./pages/admin/Bookinghistory";
@@ -19,80 +25,36 @@ import AddEvent from "./pages/admin/AddEvent";
 import EventType from "./pages/admin/EventType";
 import Profile from "./pages/admin/Profile";
 
-// User Components
-import UsersNavbar from "./components/UsersNavbar";
-import Userhomepage from "./pages/Users/Userhomepage";
-import Userabout from "./pages/Users/Userabout";
-import UserBook from "./pages/Users/UserBook";
-import UserBookinghistory from "./pages/Users/UserBookinghistory";
-import Contact from "./pages/Users/contact";
-import Userprofile from "./pages/Users/Userprofile";
-import Payment from "./pages/Users/Payment";
-
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <ToastContainer />
       <Routes>
-        {/* Public Routes (With Navbar & Footer) */}
-        <Route
-          path="/*"
-          element={
-            <>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/aboutPage" element={<About />} />
-                <Route path="/category" element={<Category />} />
-              </Routes>
-              <Footer />
-            </>
-          }
-        />
+        {/* Public Routes */}
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="aboutPage" element={<About />} />
+          <Route path="category" element={<Category />} />
+          <Route path="book" element={<UserBook />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="bookinghistory" element={<UserBookinghistory />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="profile" element={<Userprofile />} />
+        </Route>
 
-        {/* Admin Routes (With Admin Navbar, No Footer) */}
-        <Route
-          path="/admin/*"
-          element={
-            <>
-              <AdminNavbar />
-              <Routes>
-                <Route path="/dashboard" element={<AdminDashboard />} />
-                <Route path="/UserList" element={<UserList />} />
-                <Route path="/Adminabout" element={<Adminabout />} />
-                <Route path="/Bookinghistory" element={<Bookinghistory />} />
-                <Route path="/venue" element={<Venue />} />
-                <Route path="/AddEvent" element={<AddEvent />} />
-                <Route path="/EventType" element={<EventType />} />
-                <Route path="/Profile" element={<Profile />} />
-              </Routes>
-              <Footer />
-            </>
-          }
-        />
-
-        {/* User Routes (With Users Navbar & Footer) */}
-        <Route
-          path="/Users/*"
-          element={
-            <>
-              <UsersNavbar />
-              <Routes>
-                <Route path="/Userhomepage" element={<Userhomepage />} />
-                <Route path="/Userabout" element={<Userabout />} />
-                <Route path="/UserBook" element={<UserBook />} />
-                <Route path="/UserBookinghistory" element={<UserBookinghistory />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/Userprofile" element={<Userprofile />} />
-                <Route path="/Payment" element={<Payment />} />
-              </Routes>
-              <Footer />
-            </>
-          }
-        />
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="users" element={<UserList />} />
+          <Route path="about" element={<Adminabout />} />
+          <Route path="booking-history" element={<Bookinghistory />} />
+          <Route path="venue" element={<Venue />} />
+          <Route path="add-event" element={<AddEvent />} />
+          <Route path="event-type" element={<EventType />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
